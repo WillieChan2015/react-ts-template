d479271471d105d8f6b2824f38fcb5f0057bf0e8
Willie Chen <willie.chen@jodoinc.com>
2020-04-01 10:45:02 +0800

    chore: 基础模版

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const renderMethod = (module as any).hot ? ReactDOM.render : ReactDOM.hydrate;
+const render = (Component: any) => (
+  renderMethod(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  )
 );
+
+render(App);
+
+if ((module as any).hot) {
+  (module as any).hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(NextApp);
+  });
+}
+
+// ReactDOM.render(
+//   // <React.StrictMode>
+//     <App />,
+//   // </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
