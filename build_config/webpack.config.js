b529c528ff477b0a46d3529386ad7fe29bc1f62b
Willie Chen <willie.chen@jodoinc.com>
2020-04-02 16:37:26 +0800

    update: node-sass & port & alias

--- build_config/webpack.config.js
+++ build_config/webpack.config.js
@@ -293,6 +293,8 @@ module.exports = function(webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
+        '@': path.resolve(__dirname, '../src'),
+        'views': path.resolve(__dirname, '../src/views'),
         // Allows for better profiling with ReactDevTools
         ...(isEnvProductionProfile && {
           'react-dom$': 'react-dom/profiling',
